5a046f5330994e21ee5c88eb22b0982e8dbf9f38
Vraj Pandya <pandyavraj2005@gmail.com>
2026-08-31 11:07:15 +0530

    Improve iPhone browser compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,22 @@
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import tailwindcss from "@tailwindcss/vite";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import { viteSingleFile } from "vite-plugin-singlefile";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss(), viteSingleFile()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "src"),
+//     },
+//   },
+// });
 import path from "path";
 import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
@@ -10,10 +29,19 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteSingleFile(),
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+
+  build: {
+    target: "es2018",
   },
 });
